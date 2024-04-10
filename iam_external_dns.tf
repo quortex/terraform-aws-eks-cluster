7a974f5451219771d88745f8e9cb2c89c50bc0bb
Vincent Marguerie <24724195+vincentmrg@users.noreply.github.com>
2024-04-10 17:35:57 +0200

    Add configuration for external-dns IAM

---
+++ iam_external_dns.tf
@@ -0,0 +1,83 @@
+/**
+ * Copyright 2020 Quortex
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+*/
+
+locals {
+  handle_iam_external_dns = var.handle_iam_resources && var.handle_iam_external_dns
+}
+
+resource "aws_iam_role" "external_dns" {
+  count       = local.handle_iam_external_dns ? 1 : 0
+  name        = var.external_dns_role_name
+  description = "IAM Role required for external-dns."
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = {
+          Federated = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:oidc-provider/${local.cluster_oidc_issuer}"
+        }
+        Action = "sts:AssumeRoleWithWebIdentity"
+        Condition = {
+          StringEquals = {
+            "${local.cluster_oidc_issuer}:aud" : "sts.amazonaws.com"
+            "${local.cluster_oidc_issuer}:sub" : "system:serviceaccount:${var.external_dns_sa.namespace}:${var.external_dns_sa.name}"
+          }
+        }
+      }
+    ]
+  })
+
+  tags = var.tags
+}
+
+resource "aws_iam_policy" "external_dns" {
+  count       = local.handle_iam_external_dns ? 1 : 0
+  description = "The policy required for external-dns."
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        "Effect" : "Allow",
+        "Action" : [
+          "route53:ChangeResourceRecordSets"
+        ],
+        "Resource" : [
+          "arn:aws:route53:::hostedzone/*"
+        ]
+      },
+      {
+        "Effect" : "Allow",
+        "Action" : [
+          "route53:ListHostedZones",
+          "route53:ListResourceRecordSets",
+          "route53:ListTagsForResource"
+        ],
+        "Resource" : [
+          "*"
+        ]
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "external_dns" {
+  count      = local.handle_iam_external_dns ? 1 : 0
+  policy_arn = aws_iam_policy.external_dns[0].arn
+  role       = aws_iam_role.external_dns[0].name
+}

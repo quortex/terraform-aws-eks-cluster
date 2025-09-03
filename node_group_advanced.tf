ca6d313cc3e48b147e82f24ab73106eb02c754ea
YH <y.hounsounou@quortex.io>
2025-09-03 16:45:49 +0200

    add precondition on instance_types list to display relevant error message in case of empty list

--- node_group_advanced.tf
+++ node_group_advanced.tf
@@ -82,6 +82,13 @@ locals {
 
 # One launch template per node group
 resource "aws_launch_template" "quortex_launch_tpl" {
+
+  lifecycle {
+    precondition {
+      condition     = length(each.value.instance_types) > 0
+      error_message = "node group '${each.key}' must have at least one instance type defined"
+    }
+  }
   for_each = local.filtered_node_groups_advanced
 
   name = lookup(each.value, "asg_name", "${var.cluster_name}_${each.key}")
